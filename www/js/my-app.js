e88d9b41c2b360eced8e6d510b249ac9188d9f81
Marcia <marciaemontano@gmail.com>
2023-10-30 13:15:17 -0300

    nuevas paginas

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,136 +15,264 @@ var app = new Framework7({
     // Add default routes
     routes: [
         { path: '/index/',            url: 'index.html',    },
-        { path: '/reg2/',             url: 'reg2.html',    },
-        { path: '/pag3/',             url: 'pag3.html',    },
-        { path: '/masInfo/',          url: 'masInfo.html',    },
-        { path: '/about/',            url: 'about.html',    },
+        { path: '/registro/',             url: 'registro.html',    },
+        { path: '/confirmacion/',             url: 'confirmacion.html',    },
+        { path: '/info/',          url: 'info.html',    },
+        { path: '/login/',            url: 'login.html',    },
+        { path: '/home/',            url: 'home.html',    },
+        { path: '/configuracion/',            url: 'configuracion.html',    },
+        { path: '/categorias/',            url: 'categorias.html',    },
+        { path: '/fondos/',            url: 'fondos.html',    },
+        { path: '/perfil/',            url: 'perfil.html',    },
     ]
     // ... other parameters
   });
 
 var mainView = app.views.create('.view-main');
-var nombre, apellido, mail, pass;
 
+
+var db = firebase.firestore();
+var colRoles = db.collection("ROLES");
+var colPersonas = db.collection("PERSONAS");
+var colMensaje = db.collection("MENSAJES");
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-// Option 1. Using one 'page:init' handler for all pages
-$$(document).on('page:init', function (e) {
-    // Do something here when page loaded and initialized
-    console.log(e);
-})
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log('Cargo INDEX');
+    console.log('Cargo index');
+})
+
+    $$("#btnRegistro").on('click', fnRegistro);
+    //sembrarDatos();
+    cargarUsuariosEjemplo();
+    
+
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
+    console.log('Cargo Registro');
+
+    $$("#btnFinReg").on('click', fnFinRegistro)
     
 })
-$$("#indexSeguir").on('click', fnPaso1);
 
+$$(document).on('page:init', '.page[data-name="login"]', function (e) {
+    console.log('Cargo Login');
 
-$$(document).on('page:init', '.page[data-name="reg2"]', function (e) {
-    console.log('Cargo REG2');
+    $$("#btnInicioSesion").on('click', fnIniciarSesion)
+    
+})
+    
+$$(document).on('page:init', '.page[data-name="confirmacion"]', function (e) {
+    console.log('Cargo Confirmacion');
 
-    $$("#reg2Seguir").on('click', fnPaso2)
+    $$("#pagConfirmacionNombre").html(nombre);
+    $$("#pagConfirmacionEmail").html(email);
+
 })
 
-$$(document).on('page:init', '.page[data-name="pag3"]', function (e) {
-    console.log('Cargo PAG3');
-
-    $$("#pag3Nombre").html(nombre);
-    $$("#pag3Mail").html(mail);
-
-
-})
-
-$$(document).on('page:init', '.page[data-name="masInfo"]', function (e) {
-    console.log('Cargo masInfo');
+$$(document).on('page:init', '.page[data-name="info"]', function (e) {
+   
+    cargarDatosUsuarioLogueado();
+    console.log('Cargo Info');
 })
 
 $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     console.log('Cargo ABOUT');
 })
 
+/* SEMBRADO */  
+function sembrarDatos() {
 
+    var dato = { rol: "Desarrollador/a", color: "Verde" }
+    var miId = "DEV";
+    colRoles.doc(miId).set(dato)
+    .then( function(docRef) {
+        console.log("Doc creado con el id: " + docRef.id);
+    })
+    .catch(function(error) {
+        console.log("Error: " + error);
+    })
+}
 
 
 
 
 /* MIS FUNCIONES */
-function fnPaso1() {
-    mail = $$('#indexEmail').val();
-    pass = $$('#indexPassword').val();
+var nombre, apellido, email, clave;
+//CODIGO DE LOGIN:
+    // cada un@ pone su magia para recuperar el mail y la clave de un form...
 
-    if (mail != "" && pass != "") {
-        // pasar a reg2
-        mainView.router.navigate("/reg2/");
-    } else {
-        $$("#indexMensaje").html("Completá el <b>Email y Clave</b>");
+    function fnIniciarSesion() {
+        email = $$("#loginEmail").val();
+        clave = $$("#loginClave").val();
+
+        if (email!="" && clave!="") {
+
+            firebase.auth().signInWithEmailAndPassword(email, clave)
+              .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                console.log("Bienvenid@!!! " + email);
+
+                mainView.router.navigate('/home/');
+                // ...
+              })
+              .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.error(errorCode);
+                    console.error(errorMessage);
+              });
+        }
     }
-}
-
-function fnPaso2() {
-    nombre = $$('#reg2Nombre').val();
-    apellido = $$('#reg2Apellido').val();
-
-    if (nombre != "" && apellido != "") {
-        // pasar a reg2
-        mainView.router.navigate("/pag3/");
-    } else {
-        $$("#reg2Mensaje").html("Completá el <b>Nombre y Apellido</b>");
-    }
-}
+    
+    
 
     //CÓDIGO PARA EL REGISTRO
 
     // cada un@ pone su magia para recuperar el mail y la clave de un form...
-    var emailDelUser = "elvalor@delmail.com";
-    var passDelUser = "1234567890";
+    function fnRegistro() {
+        email = $$("#indexEmail").val();
+        clave = $$("#indexClave").val();
+    
+        if (email!="" && clave!="") {
 
-        firebase.auth().createUserWithEmailAndPassword(emailDelUser, passDelUser)
-        .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        console.log("Bienvenid@!!! " + emailDelUser);
-        // ...
-        mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
-        })
-        .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
+            firebase.auth().createUserWithEmailAndPassword(email, clave)
+                
+                .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                console.log("Bienvenid@!!! " + email);
+                // ...
+                mainView.router.navigate('/registro/');
+                })
+                .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
 
-        console.error(errorCode);
-        console.error(errorMessage);
-
-        if (errorCode == "auth/email-already-in-use") {
-            console.error("el mail ya esta usado");
-        }
-
-        // ..
-        });
-
-    //CODIGO DE LOGIN:
-    // cada un@ pone su magia para recuperar el mail y la clave de un form...
-    var emailDelUser = "elvalor@delmail.com";
-    var passDelUser = "1234567890";
-
-        firebase.auth().signInWithEmailAndPassword(emailDelUser, passDelUser)
-        .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-        console.log("Bienvenid@!!! " + emailDelUser);
-        // ...
-        })
-
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-
-            console.error(errorCode);
+                console.error(errorCode);
                 console.error(errorMessage);
-        });
+
+                if (errorCode == "auth/email-already-in-use") {
+                    console.error("Este email ya esta en uso");
+                }
+            })
+        };
+    }
+
+
+    function fnFinRegistro() {
+        nombre = $$("#regNombre").val();
+        apellido = $$("#regApellido").val();
+    
+        if (nombre!="" && apellido!="") {
+            mainView.router.navigate("/confirmacion/")
+    
+            datos = { nombre: nombre, apellido: apellido, rol: "DEV" }
+            elID = email;
+    
+            colPersonas.doc(elID).set(datos)
+            .then( function(docRef) {
+               mainView.router.navigate("/confirmacion/") 
+            })
+            .catch(function(error) {
+                console.log("Error: " + error);
+            })
+    
+    
+        }
+    }
+
+    function cargarUsuariosEjemplo() {
+        colPersonas.get()
+        .then( function(qs) {
+            qs.forEach( function(elDoc) {
+                nombre = elDoc.data().nombre;
+                apellido = elDoc.data().apellido;
+                rol = elDoc.data().rol;
+                email = elDoc.id;
+                $$("#listaUsuarios").append("<hr>" + nombre + " / " + apellido + " / " + rol + " / " + email);
+            } )
+        })
+        .catch(function(error) {
+            console.log("Error: " + error);
+        })
+    
+    }
+    
+    function cargarDatosUsuarioLogueado() {
+        colPersonas.doc(email).get()
+        .then( function(unDoc) {
+            //console.log(unDoc);
+            nombre = unDoc.data().nombre;
+            apellido = unDoc.data().apellido;
+            rol = unDoc.data().rol;
+            email = unDoc.id;
+            $$("#infoDatos").html("<hr>" + nombre + " / " + apellido + " / " + rol + " / " + email);
+        } )
+        .catch(function(error) {
+            console.log("Error: " + error);
+        })
+    }
+
+/*<script>
+  export default (props, { $f7, $on }) => {
+    const photos = [
+      {
+        url: 'img/logo.png',
+        caption: 'Amazing beach in Goa, India'
+      },
+      'http://placekitten.com/1024/1024',
+      'img/lock.jpg',
+      {
+        url: 'img/monkey.jpg',
+        caption: 'I met this monkey in Chinese mountains'
+      },
+      {
+        url: 'img/mountains.jpg',
+        caption: 'Beautiful mountains in Zhangjiajie, China'
+      }
+    ];
+    const thumbs = [
+      'img/beach.jpg',
+      'http://placekitten.com/1024/1024',
+      'img/lock.jpg',
+      'img/monkey.jpg',
+      'img/mountains.jpg'
+    ]
+
+    let page;
+    
+    const openPage = () => {
+      page.open();
+    }
+   
+    $on('pageInit', () => {
+      // Create PBs when page init
+      page = $f7.photoBrowser.create({
+        photos: photos,
+        thumbs: thumbs,
+        type: 'page',
+        backLinkText: 'Back',
+      });
+      
+        
+    $on('pageBeforeRemove', () => {
+      // Destroy PBs on page remove
+      standalone.destroy();
+      popup.destroy();
+      page.destroy();
+      standaloneDark.destroy();
+      popupDark.destroy();
+      pageDark.destroy();
+    });
+
+    return $render;
+  };
+</script>*/
